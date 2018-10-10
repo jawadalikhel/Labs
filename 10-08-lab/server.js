818d903e0e9c5b8cd229e8c3b550c9b27486a941
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-10-10 11:52:41 -0500

    commit 1- created array

--- 10-08-lab/server.js
+++ 10-08-lab/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 
-const greeting = 'Hello you ';
+// const greeting = 'Hello you ';
 
-app.get('/greeting', (request, response) =>{
-	response.send(greeting);
-});
+// app.get('/greeting', (request, response) =>{
+// 	response.send(greeting);
+// });
 
-app.get('/greeting/:name', (request, response) =>{
-	response.send(greeting + request.params.name);
-})
+// app.get('/greeting/:name', (request, response) =>{
+// 	response.send(greeting + request.params.name);
+// })
+
+
+// app.get('/tip/:total/:tipPercentage', (request, response) =>{
+// 	response.send(total * tipPercentage);
+// })
 
 
 app.listen(3000, () =>{
-	console.log('I am listening');
+	console.log('I am listening');		
 })
